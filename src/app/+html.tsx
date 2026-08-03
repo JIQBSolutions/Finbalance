314699d1be008458f002bb42470062681a4ca263
pal221 <paul.mostacho52@gmail.com>
2026-08-03 13:33:05 -0600

    Add Root component for HTML structure with Clarity script integration

---
+++ src/app/+html.tsx
@@ -0,0 +1,29 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import type { ReactNode } from 'react';
+
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xnw0lvol2y");
+            `,
+          }}
+        />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
